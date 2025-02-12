d1b49deef071e67dc6e609202db0fcaa9975fad2
ReallyLastOne <reallylastone@op.pl>
2025-02-12 17:51:06 +0100

    flyway migration

--- src/main/resources/db/migration/V0.0.1__init.sql
+++ src/main/resources/db/migration/V0.0.1__init.sql
@@ -1,4 +1,3 @@
-drop table finnhub_trades;
 create table finnhub_trades
 (
 	c varchar(11),
@@ -8,7 +7,6 @@ create table finnhub_trades
     v numeric
 );
 
-drop table trade_statistics;
 create table trade_statistics
 (
     start_timestamp timestamp with time zone not null unique,
@@ -23,35 +21,18 @@ create table trade_statistics
 );
 create unique index idx_start_tmstp_end_tmstp on trade_statistics(start_timestamp, end_timestamp);
 
-drop table leader;
 create table leader
 (
     id int primary key,
     renew_time timestamp with time zone,
     acquire_time timestamp with time zone,
     process_id bigint
-)
-insert
-	into
+);
+
+insert into
 	leader(id,
 	renew_time,
 	acquire_time)
 values (1,
 now(),
 now());
-
-common
-    ConnectionPool
-    Registry
-finnhub
-    FinnhubMessageEvent
-    FinnhubWebSocketClient
-leadership
-    AcquireLeadershipJob
-    Leader
-statistics
-    CalculateStatisticsJob
-    TradeStatistics
-trade
-    FinnhubTrade
-    TradeWriter
